4d44279529ed25cff50d3fdd6c27062f2c58ba34
Nelson <nelsonp2n@gmail.com>
2017-10-12 14:52:40 -0300

    CRUD OK.

--- ConexaoBD/Program.cs
+++ ConexaoBD/Program.cs
@@ -12,38 +12,41 @@ namespace ConexaoBD
         static void Main(string[] args)
         {
             var bd = new BD();
+            var app = new UsuarioAplicacao();
 
-            SqlConnection conexao = new SqlConnection(@"Data Source=CODESP1855\NELSON;Initial Catalog=ExemploBD;User ID=sa;Password=codinome");
+            SqlConnection conexao = new SqlConnection(@"Data Source=DESKTOP-PF66N0S\SQLEXPRESS;Initial Catalog=ExemploBD;User ID=sa;Password=codinome");
             conexao.Open();
 
             //string queryUpdate = "UPDATE Usuario SET nome = 'Fabio' WHERE UsuarioId = 1";
             //SqlCommand cmdUpdate = new SqlCommand(queryUpdate, conexao);
             //cmdUpdate.ExecuteNonQuery();
 
-            
+            var Usuario = new Usuario();
 
-            Console.Write("Digite o nome do usuário: ");
-            string nome = Console.ReadLine();
 
-            Console.Write("Digite o cargo do usuário: ");
-            string cargo = Console.ReadLine();
+            //Console.Write("Digite o nome do usuário: ");
+            //Usuario.Nome = Console.ReadLine();
 
-            Console.Write("Digite a data de registro do usuário: ");
-            string data = Console.ReadLine();
+            //Console.Write("Digite o cargo do usuário: ");
+            //Usuario.Cargo = Console.ReadLine();
 
-            //string queryInsert = string.Format("INSERT INTO Usuario (Nome, Cargo, Data) VALUES ('{0}', '{1}', '{2}')", nome, cargo, data);
-            //bd.ExecutaComando(queryInsert);
+            //Console.Write("Digite a data de registro do usuário: ");
+            //Usuario.Data = DateTime.Parse(Console.ReadLine());
 
-            var app = new UsuarioAplicacao();
+            ////string queryInsert = string.Format("INSERT INTO Usuario (Nome, Cargo, Data) VALUES ('{0}', '{1}', '{2}')", nome, cargo, data);
+            ////bd.ExecutaComando(queryInsert);
 
-            app.Insert(nome, cargo, data);
+            //Usuario.Id = 13;
 
-            //string querySelect = "SELECT * FROM Usuario";
-            SqlDataReader dados = app.Select();
+            //app.Save(Usuario);
 
-            foreach(var i in dados)
+            //app.Delete(13);
+
+            var dados = app.SelectAll();
+
+            foreach(var usuario in dados)
             {
-                Console.WriteLine("Id:{0}, Nome:{1}, Cargo:{2}, Data:{3}", dados["UsuarioId"], dados["Nome"], dados["Cargo"], dados["Data"]);
+                Console.WriteLine("Id:{0}, Nome:{1}, Cargo:{2}, Data:{3}", usuario.Id, usuario.Nome, usuario.Cargo, usuario.Data);
             }
         }
     }
--- ConexaoBD/UsuarioAplicacao.cs
+++ ConexaoBD/UsuarioAplicacao.cs
@@ -11,11 +11,11 @@ namespace ConexaoBD
     {
         private BD bd;
 
-        public void Insert(string nome, string cargo, string data)
+        private void Insert(Usuario Usuario)
         {
             var strQuery = "";
             strQuery += "INSERT INTO Usuario (Nome, Cargo, Data)";
-            strQuery += string.Format(" VALUES ('{0}', '{1}', '{2}')", nome, cargo, data);
+            strQuery += string.Format(" VALUES ('{0}', '{1}', '{2}')", Usuario.Nome, Usuario.Cargo, Usuario.Data);
 
             using (bd = new BD())
             {
@@ -23,14 +23,69 @@ namespace ConexaoBD
             }
         }
 
-        public SqlDataReader Select()
+        private void Update(Usuario Usuario)
         {
-            var strQuery = "SELECT * FROM Usuario";
+            var strQuery = "";
+            strQuery += "UPDATE Usuario SET ";
+            strQuery += string.Format("Nome = '{0}',", Usuario.Nome);
+            strQuery += string.Format("Cargo = '{0}',", Usuario.Cargo);
+            strQuery += string.Format("Data = '{0}'", Usuario.Data);
+            strQuery += string.Format("WHERE UsuarioId = '{0}' ", Usuario.Id);
 
             using (bd = new BD())
             {
-                return bd.ExecutaComandoComRetorno(strQuery);
+                bd.ExecutaComando(strQuery);
             }
+        }
+
+        public void Save(Usuario Usuario)
+        {
+            if (Usuario.Id > 0)
+            {
+                Update(Usuario);
+            }
+            else
+                Insert(Usuario);
+        }
+
+        public void Delete(int id)
+        {
+            using (bd = new BD())
+            {
+                var strQuery = string.Format("DELETE FROM Usuario WHERE UsuarioId = {0}", id);
+                bd.ExecutaComando(strQuery);
+            }
+        }
+
+        public List<Usuario> SelectAll()
+        {
+            using (bd = new BD())
+            {
+                var strQuery = "SELECT * FROM Usuario";
+                var retorno =  bd.ExecutaComandoComRetorno(strQuery);
+                return ListarUsuario(retorno);
+            }
+        }
+
+        private List<Usuario>ListarUsuario(SqlDataReader Reader)
+        {
+
+            var Usuario = new List<Usuario>();
+
+            while(Reader.Read())
+            {
+                var TempUsuario = new Usuario()
+                {
+                    Id = int.Parse(Reader["UsuarioId"].ToString()),
+                    Nome = Reader["Nome"].ToString(),
+                    Cargo = Reader["Cargo"].ToString(),
+                    Data = DateTime.Parse(Reader["Data"].ToString())
+                };
+
+                Usuario.Add(TempUsuario);
+            }
+            Reader.Close();
+            return Usuario;
         }
     }
 }
